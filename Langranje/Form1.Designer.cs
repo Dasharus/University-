d3be9ee2d88a8adb9a03bf529df97aed9708578d
Dasha <62296099+Dasharus@users.noreply.github.com>
2020-09-18 18:01:53 +0300

    Add files via upload

---
+++ Langranje/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Langranje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.nTextbox = new System.Windows.Forms.TextBox();
+            this.xTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.main_function = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lagranje_function = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nTextbox
+            // 
+            this.nTextbox.BackColor = System.Drawing.Color.MistyRose;
+            this.nTextbox.Location = new System.Drawing.Point(111, 27);
+            this.nTextbox.Name = "nTextbox";
+            this.nTextbox.Size = new System.Drawing.Size(63, 22);
+            this.nTextbox.TabIndex = 0;
+            // 
+            // xTextbox
+            // 
+            this.xTextbox.BackColor = System.Drawing.Color.MistyRose;
+            this.xTextbox.Location = new System.Drawing.Point(111, 66);
+            this.xTextbox.Name = "xTextbox";
+            this.xTextbox.Size = new System.Drawing.Size(63, 22);
+            this.xTextbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введіть n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введіть x";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            legend2.Position.Auto = false;
+            legend2.Position.Height = 15F;
+            legend2.Position.Width = 22.31293F;
+            legend2.Position.X = 74.68707F;
+            legend2.Position.Y = 3F;
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(303, 12);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series4.Legend = "Legend1";
+            series4.Name = "Main function";
+            series5.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            series5.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Color = System.Drawing.Color.Red;
+            series5.Legend = "Legend1";
+            series5.Name = "Lagranje polinom";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.Name = "points";
+            series6.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(736, 421);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1.Location = new System.Drawing.Point(159, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(87, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(25, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Значення в точці:\r\n";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.main_function,
+            this.lagranje_function});
+            this.dataGridView1.Location = new System.Drawing.Point(303, 450);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(595, 183);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.MinimumWidth = 6;
+            this.X.Name = "X";
+            this.X.Width = 125;
+            // 
+            // main_function
+            // 
+            this.main_function.HeaderText = "Main Function";
+            this.main_function.MinimumWidth = 6;
+            this.main_function.Name = "main_function";
+            this.main_function.Width = 125;
+            // 
+            // lagranje_function
+            // 
+            this.lagranje_function.HeaderText = "Lagranje function";
+            this.lagranje_function.MinimumWidth = 6;
+            this.lagranje_function.Name = "lagranje_function";
+            this.lagranje_function.Width = 125;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(166, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Рівновіддалені вузли";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(140, 21);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Вузли Чебишева";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вхідні вузли:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Location = new System.Drawing.Point(34, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "1/ ( 1+25x^2)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.Location = new System.Drawing.Point(34, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "ln(x+2)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Обрахувати:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.MistyRose;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(276, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 654);
+            this.flowLayoutPanel1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 475);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1051, 654);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xTextbox);
+            this.Controls.Add(this.nTextbox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Interpolation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nTextbox;
+        private System.Windows.Forms.TextBox xTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn main_function;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lagranje_function;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Langranje/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Langranje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double LangrF(int n, double x_main)
+        {
+            double a = -1;
+            double b = 1;
+            double h = (b - a) / n;
+            double[] x = new double[n + 1];
+            if (radioButton1.Checked)                   // Рахуємо масив інтерполяційних вузлів
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    x[i] = a + i * h;
+                }
+            }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    x[i] = ((a + b) / 2.0) + ((b - a) / 2.0 )* Math.Cos(((2.0 * i + 1) * 3.14) / (2.0 * (n + 1)));
+                }
+            }
+            double[] f = new double[n + 1];
+            for (int i = 0; i <= n; i++)            // Рахуємо масив значень функцій 
+            {
+                f[i] = 1 / (1 + 25 * x[i] * x[i]);
+            }
+            double l;
+            double lagr = 0;
+            for (int i = 0; i <= n; i++)                // Ланграж 
+            {
+                l = 1;
+                for (int j = 0; j <= n; j++)
+                {
+                    if (j != i)
+                        l *= (x_main - x[j]) / (x[i] - x[j]);
+                }
+                lagr += l * f[i];
+            }
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                f[i] = f[i] - f[i] % 0.001;
+                chart1.Series["points"].Points.AddXY(x[i], f[i]);
+            }
+            double result = Math.Abs(1 / (1 + 25 * x_main * x_main) - lagr);
+            textBox2.Text = Convert.ToString(result);
+
+            return lagr;
+        }
+
+        public double LagranjeG(int n, double x_main)
+        {
+            double a = -1;
+            double b = 1;
+            double h = (b - a) / n;
+            double[] x = new double[n + 1];
+
+
+            if (radioButton1.Checked)                     // Рахуємо масив інтерполяційних вузлів
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    x[i] = a + i * h;
+                }
+            }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    x[i] = ((a + b) / 2.0) + ((b - a) / 2.0) * Math.Cos(((2.0 * i + 1) * 3.14) / (2.0 * (n + 1)));
+                }
+            }
+
+            double[] g = new double[n + 1];                  // Рахуємо масив значень функцій 
+            for (int i = 0; i <= n; i++)
+            {
+                g[i] = Math.Log(x[i] + 2);
+            }
+
+     
+
+            double l;
+            double lagr = 0;
+            for (int i = 0; i <= n; i++)                     // Ланграж 
+            {
+                l = 1;
+                for (int j = 0; j <= n; j++)
+                {
+                    if (j != i)
+                        l *= (x_main - x[j]) / (x[i] - x[j]);
+                }
+                lagr += l * g[i];
+            }
+
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                g[i] = g[i] - g[i] % 0.001;
+                chart1.Series["points"].Points.AddXY(x[i], g[i]);
+            }
+
+            double result = Math.Abs(Math.Log(x_main + 2) - lagr);
+            textBox2.Text = Convert.ToString(result);
+            return lagr;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            chart1.Series["Main function"].Points.Clear();
+            chart1.Series["Lagranje polinom"].Points.Clear();
+            chart1.Series["points"].Points.Clear();
+
+
+            int n1 = Convert.ToInt32(nTextbox.Text);
+            int n2 = 100;
+            double x_main = Convert.ToDouble(xTextbox.Text);
+            double a = -1;
+            double b = 1;
+            double h = (b - a) / n2;
+            double[] x = new double[n2 + 1];
+            double[] f = new double[n2 + 1];
+            if (radioButton1.Checked)
+                for (int i = 0; i <= n2; i++)
+                {
+                    x[i] = a + i * h;
+
+                }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i <= n2; i++)
+                {
+                    x[i] = ((a + b) / 2.0) + ((b - a) / 2.0) * Math.Cos(((2.0 * i + 1) * 3.14) / (2.0 * (n2 + 1)));
+                }
+            }
+
+            for (int i = 0; i <= n2; i++)
+            {
+                f[i] = 1 / (1 + 25 * x[i] * x[i]);
+                chart1.Series["Main function"].Points.AddXY(x[i], f[i]);
+            }
+      
+
+            double lag = LangrF(n1, x_main);
+            textBox1.Text = Convert.ToString(lag);
+
+            double[] lag1 = new double[n2 + 1];
+            for (int i = 0; i <= n2; i++)
+            {
+                lag1[i] = LangrF(n1, x[i]);
+                chart1.Series["Lagranje polinom"].Points.AddXY(x[i], lag1[i]);
+            }
+            for (int i = 0; i <= n2; i++)
+            {
+                dataGridView1.Rows.Add(x[i], f[i], lag1[i]);
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            chart1.Series["Main function"].Points.Clear();
+            chart1.Series["Lagranje polinom"].Points.Clear();
+            chart1.Series["points"].Points.Clear();
+
+            int n1 = Convert.ToInt32(nTextbox.Text);
+            int n2 = 100;
+            double x_main = Convert.ToDouble(xTextbox.Text);
+            double a = -1;
+            double b = 1;
+            double h = (b - a) / n2;
+            double[] x = new double[n2 + 1];
+            double[] f = new double[n2 + 1];
+            if (radioButton1.Checked)
+                for (int i = 0; i <= n2; i++)
+                {
+                    x[i] = a + i * h;
+
+                }
+            if (radioButton2.Checked)
+            {
+                for (int i = 0; i <= n2; i++)
+                {
+                    x[i] = (a + b) / 2.0 + (b - a) / 2.0 * Math.Cos(((2.0 * i + 1) * 3.14) / (2.0 * (n2 + 1)));
+                }
+            }
+            for (int i = 0; i <= n2; i++)
+            {
+                f[i] = Math.Log(x[i] + 2);
+                chart1.Series["Main function"].Points.AddXY(x[i], f[i]);
+            }
+            double lag = LagranjeG(n1, x_main);
+            textBox1.Text = Convert.ToString(lag);
+
+            double[] lag1 = new double[n2 + 1];
+            for (int i = 0; i <= n2; i++)
+            {
+                lag1[i] = LagranjeG(n1, x[i]);
+                chart1.Series["Lagranje polinom"].Points.AddXY(x[i], lag1[i]);
+            }
+            for (int i = 0; i <= n2; i++)
+            {
+                dataGridView1.Rows.Add(x[i], f[i], lag1[i]);
+            }
+        }
+    }
+
+
+}
